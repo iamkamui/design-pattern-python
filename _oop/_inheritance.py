5019856a59d9322e2ca970dcf4edbe4c2a6cf380
Pedro Augusto <pagb.pedro@hotmail.com>
2024-07-07 20:11:05 -0300

    add inheritance translation and upgrade example

--- _oop/_inheritance.py
+++ _oop/_inheritance.py
@@ -1,36 +1,57 @@
-class Animal:
-    def __init__(self, name, sex, weight, color):
-        self.name = name
-        self.sex = sex
-        self.weight = weight
+"""
+en_US:
+    If you want to create new classes with a bit diferents that one that already exists,
+    don't have necessity to duplicate your code, you can extends the existent class and create
+    the adicional funcionality inside of subclass.
+
+pt_BR:
+    Se você deseja criar novas classes um pouco diferentes daquela que já existe,
+    não há necessidade de duplicar seu código, você pode estender a classe existente e criar
+    a funcionalidade adicional dentro da subclasse.
+"""
+
+from typing import Dict
+
+
+class Vehicle:
+    def __init__(self, model: str, engine: str, year: str, color: str):
+        self.model = model
+        self.engine = engine
+        self.year = year
         self.color = color
 
-    def breath(self):
-        print(f"{self.name} breathing")
+    def start_engine(self) -> str:
+        return f"Starting {self.engine} engine ..."
 
-    def eat(self, food):
-        print(f"{self.name} eating {food}")
-
-    def run(self, destination):
-        print(f"{self.name} running to {destination}")
-
-    def sleep(self, hours):
-        print(f"{self.name} sleeping for {hours} hours")
-
-
-class Cat(Animal):
-    def __init__(self):
-        super().__init__()
-        self.isNasty = bool
-
-    def meow(self):
-        print("meow")
+    def _base_info(self) -> Dict:
+        return {
+            "model": self.model,
+            "engine": self.engine,
+            "year": self.year,
+            "color": self.color,
+        }
 
 
-class Dog(Animal):
-    def __init__(self):
-        super().__init__()
-        self.best_friend = "Human"
+class Motorcyle(Vehicle):
+    def __init__(self, lever_type: str, model: str, engine: str, year: str, color: str):
+        super().__init__(model, engine, year, color)
+        self.lever_type = lever_type
 
-    def bark(self):
-        print("Wouf")
+    def wheelie(self) -> str:
+        return "Wheeling..."
+
+    def get_bike_info(self) -> Dict:
+        base_info = self._base_info()
+        base_info.update({"lever_type": self.lever_type})
+        return base_info
+
+
+class Car(Vehicle):
+    def __init__(self, body: str, model: str, engine: str, year: str, color: str):
+        super().__init__(model, engine, year, color)
+        self.body = body
+
+    def get_car_info(self) -> Dict:
+        base_info = self._base_info()
+        base_info.update({"body": self.body})
+        return base_info
